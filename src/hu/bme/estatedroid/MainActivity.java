2d4aec3a0d124ab3efb77074b5c21062ac5e20ca
Szabó Tamás <saintszt@gmail.com>
2013-04-08 23:33:39 +0200

    Alap

---
+++ src/hu/bme/estatedroid/MainActivity.java
@@ -0,0 +1,38 @@
+package hu.bme.estatedroid;
+
+import hu.bme.estatedroid.activity.PrefsActivity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
+
+public class MainActivity extends SherlockActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getSupportMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		int itemId = item.getItemId();
+		if (itemId == R.id.action_settings) {
+			Intent intent = new Intent(getBaseContext(), PrefsActivity.class);
+			startActivity(intent);
+			return true;
+		} else {
+			return super.onOptionsItemSelected(item);
+		}
+	}
+
+}
